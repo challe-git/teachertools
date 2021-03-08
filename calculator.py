c48dfd53b8d5c9bae6bbc445db0983fe948b84c6
challe-git <76874224+challe-git@users.noreply.github.com>
2021-03-08 13:11:16 +0100

    Delete calculator.py

--- calculator.py
+++
@@ -1,8 +0,0 @@
-def calculate(oper, n1, n2):
-  if oper == "+": summary = n1 + n2
-  if oper == "-": summary = n1 - n2
-  if oper == "*": summary = n1 * n2
-  if oper == "/": summary = n1 / n2
- 
-calculate(char(input("pick a operator ( + , - , * , / )\n")), float(input("pick a number:\n")), float(input("pick a number:\n")))
-print(f"summary = {calculate.summary}")
